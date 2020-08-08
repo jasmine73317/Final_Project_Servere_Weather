50c8e2340138c54e9b6f01cb6415b35ef8423938
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-08-08 14:46:32 -0500

    beauty

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,15 +3,16 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# A welcome message to test our server
-@app.route('/')
-def hello():
-    return "<h1>Welcome to our server!</h1>"
 
 # type in 'index' at the end of url to see sample test *** note flask needs a templates folder 
-@app.route('/index/')
-def index(name=None):
-    return render_template('test.html', name=name)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# A welcome message to test our server
+@app.route('/summary/')
+def summary():
+     return render_template('generic.html') 
 
 if __name__ == '__main__':
     
