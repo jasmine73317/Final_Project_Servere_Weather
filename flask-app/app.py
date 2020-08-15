6ab48de91017c71fd2e99a0aed51f40f01356df9
Vijay S <37852613+vijay043077@users.noreply.github.com>
2020-08-15 00:20:20 -0500

    update models

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,19 +1,23 @@
 # app.py    
 from flask import Flask, request, jsonify, render_template
+from joblib import load
 
 app = Flask(__name__)
 
+# model1 = load("flask-app/models/Humidity_Regressions.ipynb") file too large
+# model2 = load("flask-app/models/Humidity_Trees.joblib") file too large
 
 # type in 'index' at the end of url to see sample test *** note flask needs a templates folder 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# A welcome message to test our server
+# second page setup
 @app.route('/summary/')
 def summary():
      return render_template('generic.html') 
 
 if __name__ == '__main__':
-    
+    # print(model1, model2)
     app.run(debug=True, port=5000) 
+    
