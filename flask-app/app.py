91f909ba56ccb5906ba06bacf9a16049d9583c31
rstankovic <cschristianstankovic@gmail.com>
2020-08-15 08:19:28 -0500

    new branch

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,14 +1,24 @@
 # app.py    
 from flask import Flask, request, jsonify, render_template
 from flask import request 
+import os
 
 app = Flask(__name__)
 
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 # type in 'index' at the end of url to see sample test *** note flask needs a templates folder 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/lstmpredict', methods = ['GET','POST'])
+def weatherpredict():
+    #print('.....')
+    prediction_value = None
+    if request.method == 'GET':
+        return render_template('weatherpredict.html')
 
 #model input(?)
 @app.route('/resource/', methods = ['POST'])
